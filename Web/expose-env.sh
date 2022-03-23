a96b619c61df719ff9971e9e23cb7d877dc3f414
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2022-03-23 10:45:00 +0100

    Changed background color

--- Web/expose-env.sh
+++ Web/expose-env.sh
@@ -2,4 +2,4 @@ mkdir /usr/share/nginx/html/config/
 echo "YAY ${API_URL}"
 echo "${API_URL}" > /usr/share/nginx/html/config/api-url.txt
 cat /docker-entrypoint.sh
-/docker-entrypoint.sh nginx -g daemon
+/docker-entrypoint.sh
